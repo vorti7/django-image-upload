00c7795cb4f07d0fb31863587a996375b9d14c6e
vorti <sotus195@gmail.com>
2018-11-22 05:55:25 +0000

    image processing

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.urls import reverse
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 # Create your models here.
+
+def post_image_path(instance, filename):
+    return 'posts/{}/{}'.format(instance.pk, filename)
+
+class Post(models.Model):
+    image = ProcessedImageField(
+                upload_to='posts/images',
+                processors=[ResizeToFill(300,300)],
+                format='JPEG',
+                options={'quality':90},
+            )
+    content = models.TextField()
+    
+    def get_absolute_url(self):
+        return reverse('posts:detail', kwargs={'pk':self.pk})
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from .models import Post
+from django.urls import reverse_lazy
 
 # Create your views here.
+class PostList(ListView):
+    model = Post
+    
+class PostCreate(CreateView):
+    model = Post
+    fields = ['image','content',]
+    
+class PostDetail(DetailView):
+    model = Post
+    
+class PostUpdate(UpdateView):
+    model = Post
+    fields =['image', 'content']
+    
+class PostDelete(DeleteView):
+    model = Post
+    success_url = reverse_lazy('posts:list')
